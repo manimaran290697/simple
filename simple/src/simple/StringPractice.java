6d8eefbd7ad83fd83b362c34bd84b37346a8a1ad
admin <admin@DESKTOP-E04GLGP>
2022-12-22 07:35:54 +0530

    This is first commit

---
+++ simple/src/simple/StringPractice.java
@@ -0,0 +1,13 @@
+package simple;
+
+public class StringPractice {
+	String a = "akash";
+	
+	public static void main(String[] args) {
+		
+		StringPractice a1 = new StringPractice();
+	     String a2 = a1.a;
+	     System.out.println(a2);
+	}
+
+}
